89015331a664b1da9ae56cfc626ac7eda09004ae
Franklin Mosley <franklin.p.mosley@gmail.com>
2021-10-23 09:29:40 -0400

    Code for module 01 | Introduction to Programming

---
+++ 01-introduction-to-programming/PY-01-L02/hello_world.py
@@ -0,0 +1,2 @@
+print("Hello World")
+# This program will print to the console the string "Hello World!"
---
+++ 01-introduction-to-programming/py_02_vs_03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# This is an example of differences between Python 2 and Python 3
+
+# Difference in division
+# Python 2 will round down and return an integer. This is called "integer division"
+# Python 3 will return a float. This is called "float division"
+
+print(5/2) 
+
+# Use of parentheses
+# Python 2 does not use parentheses in commands.
+# Python 3 treats commands as functions.
+
+print "Hello World"     # This only works in Python 2. Comment out for Python 3
+print("Hello World")    # Python 3 treats print as a function.
+
