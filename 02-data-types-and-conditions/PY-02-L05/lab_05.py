f332398502e6b96fa9656048007ea12c3d07ced5
Franklin Mosley <franklin.p.mosley@gmail.com>
2022-04-02 17:56:42 -0400

    Update Lab 05 code

--- 02-data-types-and-conditions/PY-02-L05/lab_05.py
+++ 02-data-types-and-conditions/PY-02-L05/lab_05.py
@@ -3,5 +3,13 @@ ip_address = input("Please enter an IP address: ")
 ip_octets = ip_address.split(".")
 
 print(f"IP Octets: {ip_octets}")
-for ip_octet in ip_octets:  # Implemented the lab with a for loop; Taught in the next class on Loops
-    print(f"{ip_octet}\t{bin(int(ip_octet))[2:]}")
+
+first = int(ip_octets[0])
+second = int(ip_octets[1])
+third = int(ip_octets[2])
+fourth = int(ip_octets[3])
+
+print(f"{first:>3} : {bin(first)[2:]:0>8}")
+print(f"{second:>3} : {bin(second)[2:]:0>8}")
+print(f"{third:>3} : {bin(third)[2:]:0>8}")
+print(f"{fourth:>3} : {bin(fourth)[2:]:0>8}")
