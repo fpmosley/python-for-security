47a956e63aa1da1920474b3fa4b542d49cb6fc15
Franklin Mosley <franklin.p.mosley@gmail.com>
2022-04-23 15:46:37 -0400

    Updated to only include the basic data types introduced in Module 1

--- 01-introduction-to-programming/python_data_types.py
+++ 01-introduction-to-programming/python_data_types.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 variable_one = "Hello"
-variable_two = "25,"
+variable_two = 25
 variable_three = 25.0
 variable_four = True
-variable_five = [1, 2, 3]
-variable_six = (1, 2, 3)
-variable_seven = {'key': "value", 'key2': "value2"}
 
 print(f"Type for 'variable_one': {type(variable_one)}")
 print(f"Type for 'variable_two': {type(variable_two)}")
 print(f"Type for 'variable_three': {type(variable_three)}")
 print(f"Type for 'variable_four': {type(variable_four)}")
-print(f"Type for 'variable_five': {type(variable_five)}")
-print(f"Type for 'variable_six': {type(variable_six)}")
-print(f"Type for 'variable_seven': {type(variable_seven)}")
+
+# Ignore everything below here
+#variable_five = [1, 2, 3]
+'''
+variable_six = (1, 2, 3)
+variable_seven = {'key': "value", 'key2': "value2"}
+'''
+
+#print(f"Type for 'variable_five': {type(variable_five)}")
+#print(f"Type for 'variable_six': {type(variable_six)}")
+#print(f"Type for 'variable_seven': {type(variable_seven)}")
