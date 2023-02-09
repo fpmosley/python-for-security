99c0d34bcc2e8ba623a0ab9aa510a50ed1d759d7
Franklin Mosley <franklin.p.mosley@gmail.com>
2023-02-09 17:49:42 -0500

    Changed names of files used for __name__ demo

--- 05-functions/code_handling/bar.py
+++ 05-functions/code_handling/bar.py
@@ -1,5 +1,5 @@
 def main():
-    print(f"util: {__name__}")
+    print(f"bar: {__name__}")
 
 
 if __name__ == '__main__':
---
+++ 05-functions/code_handling/foo.py
@@ -0,0 +1,10 @@
+import bar
+
+
+def main():
+    bar.main()
+    print(f"foo: {__name__}")
+
+
+if __name__ == '__main__':
+    main()
